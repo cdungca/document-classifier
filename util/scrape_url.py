800a7139dcac40d62042045dd783d4d0d6113414
Christian <christian.dungca@gmail.com>
2024-09-02 14:38:07 -0400

    Finalized data preparation scripts

--- util/scrape_url.py
+++ util/scrape_url.py
@@ -4,15 +4,14 @@ import ssl
 import warnings
 from openpyxl import Workbook
 import csv as csv
-
 import requests.packages.urllib3.exceptions as urllib3_exceptions
-
 warnings.simplefilter("ignore", urllib3_exceptions.InsecureRequestWarning)
 
 def extract_url(sourceurl, category):
 
     records = []
-     
+
+    # Work around for SSL Error 
     class TLSAdapter(requests.adapters.HTTPAdapter):
         def init_poolmanager(self, *args, **kwargs):
             ctx = ssl.create_default_context()
@@ -22,9 +21,6 @@ def extract_url(sourceurl, category):
             kwargs["ssl_context"] = ctx
             return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
 
-
-    
-
     with requests.session() as s:
         s.mount("https://", TLSAdapter())
 
@@ -32,37 +28,17 @@ def extract_url(sourceurl, category):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         urls = []
-
-        # xlsfilename
-        #xlsfilename = 'scrape_full.xlsx'
-        
-        # open workbook
-        #wb = Workbook(xlsfilename)
-
-        #ws = wb.index
-        
-        #ws = wb.active
-        #row = ['test1', 'test2']
-        #ws.append(row)
-        # grab active worksheet
-        #sheet = wb.active
-        #sheet.append(["test", "two"])
-
-        
+               
         
                        
-
         for link in soup.table.find_all('a'):
             url = ''
             record = {}
             
-            # check first character of string link
+            # check scraped url and process it accordingly
             if link.get('href')[0] == '/' :
-                #print("https://policy.un.org" + link.get('href'))
                 url = "https://policy.un.org" + link.get('href')
             else:
-	            #print(link.get('href'))
-                #url = link.get('href')
                 if link.get('href').find("https://www.undocs.org/en/") > -1 :
                     url = link.get('href')
                 elif link.get('href').find("https://www.undocs.org/") > -1 :
@@ -75,6 +51,7 @@ def extract_url(sourceurl, category):
 
             records.append(record)
 
+        # Write urls and category to csv
         with open(f"scrapefull.csv", 'a') as f:
             writer=csv.writer(f, delimiter=',', dialect='unix')
             for row in records:
@@ -82,14 +59,26 @@ def extract_url(sourceurl, category):
 
         records = []
             
-            
-
-        #wb.save(xlsfilename)
 
 if __name__ == "__main__":
     
-    sourceurls = ["https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A30425", "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A30425&page=1"]
-    sourcecategories = ["accountability", "accountability"]
+    # Urls from website and labeled category
+    sourceurls = ["https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A30425", 
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A30425&page=1",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29785",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29785&page=1",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29060",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29060&page=1",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29665",
+                  "https://policy.un.org/policy-all?f%5B0%5D=node%253Afield_document_topic_theme%253Aparents_all%3A29665&page=1"]
+    sourcecategories = ["accountability", 
+                        "accountability",
+                        "health and wellbeing",
+                        "health and wellbeing",
+                        "human resources",
+                        "human resources",
+                        "travel",
+                        "travel"]
 
     with open(f"scrapefull.csv", 'w') as f:
         writer=csv.writer(f, delimiter=',', dialect='unix')
